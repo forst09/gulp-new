00555b1d0b817dafcea209331a3a91eddb539266
forst09 <dforst09@gmail.com>
2023-07-31 13:03:57 +0300

    fix sass and webp

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -1,3 +1,5 @@
+import webpack from 'webpack-stream';
+
 export const js = () => {
     return app.gulp.src(app.path.src.js, { sourcemaps: app.isDev })
         .pipe(app.plugins.plumber(
@@ -6,6 +8,12 @@ export const js = () => {
                 message: "Error: <%= error.message %>"
             })
         ))
+        .pipe(webpack({
+            mode: app.isBuild ? 'production' : 'development',
+            output: {
+                filename: 'app.min.js'
+            }
+        }))
         .pipe(app.gulp.dest(app.path.build.js))
         .pipe(app.plugins.browsersync.stream());
 }
